1e638d52736dcfa502cca850eed27ffd4c14e92b
calobetoleon <cleon@fuxion.net>
2022-09-09 08:37:26 -0500

    classes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Navbar from "./Navbar"
 import Home from "./Home"
+//#21 
+import { BrowserRouter as Router, HashRouter, Route, Switch} from 'react-router-dom'
+import Create from "./Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
-
+    <Router> 
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+         <Switch> 
+          <Route exact path='/'> 
+            <Home/>
+          </Route>
+          <Route path='/create'> 
+            <Create/>
+          </Route>
+         </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,117 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+    //let name = 'mario'
     
-    const [name, setName] = useState('mario')
-    const [age, setAge] = useState(25)
+    // create a reactive variable
+    //const [name, setName] = useState('mario') 
+    //const [age, setAge] = useState(25)
 
-    const handleClick = () => {
+    // simulating an array of blogs, each id would be unique
+    // const [blogs, setBlogs] = useState([
+    //    {title: 'My new website', body: 'lorem...', author: 'Mario', id: 1},
+    //    {title: 'welcome party', body: 'lorem...', author: 'Yoshi', id: 2},
+    //    {title: 'Web dev top tips', body: 'lorem..', author: 'Mario', id: 3}
+    //])
+
+    //const handleClick = () => {
     //    console.log('Hello tigres!')
-        setName('Luigi')
-
-    }
+    //    setName('Luigi')
+    //    setAge(30)
+    //}
 
     //const handleClickAgain = (name, e) => {
     //    console.log('hello' + name, e.target)
     //}
 
     // <button onClick={(e) => {handleClickAgain(' Castillo', e) }}>Click me again</button> 
-    // erased bellow first butto 
+    // erased bellow first button
+
+    // #12 <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blog" />
+    
+    // #13
+    //const handleDelete = (id) => {
+    //    // newBlogs is to store the filtered array temporarily. blog.id is the iteration
+    //    const newBlogs = blogs.filter(blog => blog.id !== id)
+    //    setBlogs(newBlogs)
+    //}
+    
+    // #14
+    //useEffect(() => {
+    //    console.log('use effect')
+    //    console.log(blogs)
+    //})
+
+    // #15
+    //const [name, setName] = useState('mario')
+
+    /*
+    useEffect(() => {
+        console.log('use effect')
+        //console.log(blogs)
+        console.log(name)
+    }, [name])
+    */
+
+    /*
+        return ( 
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} BlogList/>
+            <button onClick={() => setName('Luigi')}>change name</button>
+            <p>{ name }</p>
+        </div>
+     )
+    */
+
+    /*
+    // #17
+    const [blogs, setBlogs] = useState(null) // initial value to null
+
+    // #18
+    const [isPending, setIsPending] = useState(true)
+
+    // #19
+    const [error, setError] = useState(null)
+    
+    // #20 move to useFetch.js 
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if(!res.ok ) {
+                        throw Error('could not fetch the data for that resource')
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    //console.log(data)
+                    setBlogs(data)
+                    setIsPending(false)
+                    setError(null)
+                })
+                .catch(err =>{
+                    //console.log(err.message)
+                    // #19
+                    setIsPending(false)
+                    setError(err.message)
+                })
+        }, 1000)
+    }, [])
+    */
+
+    // #20
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
+            
+            { error && <div> {error} </div>}
+            { isPending && <div> Loading ...</div> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
         </div>
-     );
+     )
 }
  
 export default Home;
